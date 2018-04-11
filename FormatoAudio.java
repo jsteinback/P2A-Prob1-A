fc2791f05752da8d2d88d84105f4456b8e63701e
jsteinback <38146387+jsteinback@users.noreply.github.com>
2018-04-10 21:43:48 -0300

    Add files via upload

---
+++ FormatoAudio.java
@@ -0,0 +1,16 @@
+
+/**
+ *
+ * @author Jessica
+ */
+public interface FormatoAudio {
+    
+   public void abrir (String formato);
+   public void reproduzir ();
+   public void pausar ();
+   public void parar ();
+   public void avancar (int formato);
+   public void retornar (int formato);
+   public void liberar ();
+    
+}
---
+++ FormatoWMA.java
@@ -0,0 +1,38 @@
+
+/**
+ *
+ * @author Jessica
+ */
+public class FormatoWMA implements FormatoAudio{
+
+    @Override
+    public void abrir(String formato) {
+    }
+
+    @Override
+    public void reproduzir() {
+    }
+
+    @Override
+    public void pausar() {
+    }
+
+    @Override
+    public void parar() {
+    }
+
+    @Override
+    public void avancar(int formato) {
+    }
+
+    @Override
+    public void retornar(int formato) {
+    }
+
+    @Override
+    public void liberar() {
+    }
+
+   
+    
+}
